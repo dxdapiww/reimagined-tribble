5869a45154208f2f1d792dd1f202ca692763c854
dxdapiww <903747446@qq.com>
2020-12-08 22:15:13 +0800

    A

---
+++ Test/2020-11/2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  int n, t, count = 0, max = 0;
+  t = 0;
+  scanf("%d", &n);
+  int a[1000];
+  int b[1000] = {0};
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (a[i] == a[j]) {
+        b[i]++;
+      }
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    if (b[i] >= max) {
+      max = b[i];
+      t = i;
+    }
+  }
+  printf("%d %d", a[t], b[t]);
+  return 0;
+}
